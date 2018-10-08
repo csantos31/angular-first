9c857db6bce70966b83c7dad189f3a328cfdf2f5
Augusto Queiroz <cyntia.aparecida@hotmail.com>
2018-10-08 11:29:24 -0300

    lista

--- angular-bootstrap-example/src/app/app.component.ts
+++ angular-bootstrap-example/src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import {FormsModule, ReactiveFormsModule } from "@angular/forms";
+
 
 @Component({
   selector: 'abe-root',
@@ -7,4 +10,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-bootstrap-example';
+
+  lista:any = [];
+  constructor(){
+    //nothing tu put here yet
+  }
+
+  ngOnInit(){
+    this.lista = [
+      {
+        "id": "1",
+        "nome": "Cyntia",
+        "senha": "secret123"
+      },
+      {
+        "id": "1",
+        "nome": "Ingrid",
+        "senha": "secret321"
+      }
+    ]
+  }
+
+  functionClick(){
+    console.log('oi');
+  }
 }
+
+
